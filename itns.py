f36d0d7c81255b9b166ff3bd9be34290f3c57702
gsec <o0v0o.ix@gmail.com>
2016-12-05 19:03:03 +0100

    py-ipfsapi

--- itns.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-
-def get_hash(fname):
-    outp = subprocess.run(['ipfs', 'add', fname], stdout=subprocess.PIPE)
-    hsh = outp.stdout.decode().split()[1]
-    return(hsh)
--- test_itns.py
+++ test_itns.py
@@ -1,8 +1,20 @@
-# Let us build an api!
-import itns
+# Let us build an api! -------
+# we already have an api...
+import ipfsapi
+
+
+def test_local_handler():
+    try:
+        handler = ipfsapi.connect(host='localhost', port=5001)
+        print("imokay")
+    except:
+        handler = None
+        print("failed to create handler!")
+    return handler
 
 
 def test_hash():
-    myhash = itns.get_hash('that_mysterious_testfile')
-    assert myhash ==  "QmNoUw1sQcz68dRpNGgr46C6vHithq2Wwa7HStFzwNRvKj"
-    print(myhash)
+    handler = test_local_handler()
+    test_obj = handler.add('that_mysterious_testfile')
+    print(test_obj)
+    assert test_obj['Hash'] == "QmNU5J9iouoKLhnFMJHfZoa4yHEsfbRrLVGMqw9Nh2Ugw6"
